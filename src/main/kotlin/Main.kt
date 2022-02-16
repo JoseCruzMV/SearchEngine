a62a71e37422933403fbe955e659491bc8719f8c
Jose C Morgado Vargas <josecruzmv@outlook.com>
2022-02-16 14:06:39 -0600

    Change functionality to search in a list of people

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,63 @@
+class Person (val name: String,
+              val lastName: String = "",
+              val email: String = "")
+{
+    fun printPerson() = println("$name $lastName $email")
+}
+
 fun main() {
     getWords()
 }
 
 fun getWords() {
-    val listOfWords = getListOfWords()
-    val wordToSearch = getSearchWord()
-    findWord(listOfWords = listOfWords, searchWord = wordToSearch)
+    val listOfPeople = getData()
+    searchAction(listOfPeople = listOfPeople)
 }
 
-fun findWord(listOfWords: List<String>, searchWord: String) {
-    val word = listOfWords.indexOf(searchWord)
-    println( if (word >= 0) word + 1 else "Not Found")
+fun searchAction(listOfPeople: MutableList<Person>) {
+    println("\nEnter the number of search queries:")
+    var numberOfQueries = readLine()!!.toInt()
+    while (numberOfQueries > 0) {
+        findPerson(listOfPeople = listOfPeople)
+        numberOfQueries--
+    }
 }
 
-fun getSearchWord() = readLine()!!
+fun findPerson(listOfPeople: MutableList<Person>) {
+    var foundPerson = false
+    var personFoundHeadline = true
+    println("\nEnter data to search people:")
+    val query = readLine()!!
+    for (person in listOfPeople) {
+        if (person.name.contains(query, ignoreCase = true) ||
+            person.lastName.contains(query, ignoreCase = true) ||
+            person.email.contains(query, ignoreCase = true)
+        ) {
+            if (personFoundHeadline) {
+                println("\nPeople found:")
+                personFoundHeadline = false
+            }
+            person.printPerson()
+            foundPerson = true
+        }
+    }
+    if (!foundPerson) println("No matching people found.")
+}
 
-fun getListOfWords() = readLine()!!.split(" ")
+fun getData(): MutableList<Person> {
+    val listOfPeople = mutableListOf<Person>()
+    println("Enter the number of people:")
+    var dataSize = readLine()!!.toInt()
+    println("Enter all people: ")
+    while (dataSize > 0) {
+        val data = readLine()!!.split(" ")
+        when (data.size) {
+            1 -> listOfPeople.add(Person(name = data[0]))
+            2 -> listOfPeople.add(Person(name = data[0], lastName = data[1]))
+            else -> listOfPeople.add(Person(name = data[0], lastName = data[1], email = data[2]))
+        }
+        dataSize--
+    }
+    return listOfPeople
+}
+
