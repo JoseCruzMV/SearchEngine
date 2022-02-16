b531842b36d8884ff2e9220a94078ecfa2aec00a
Jose C Morgado Vargas <josecruzmv@outlook.com>
2022-02-15 19:26:42 -0600

    Looking for a word in a list added by user

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    getWords()
 }
+
+fun getWords() {
+    val listOfWords = getListOfWords()
+    val wordToSearch = getSearchWord()
+    findWord(listOfWords = listOfWords, searchWord = wordToSearch)
+}
+
+fun findWord(listOfWords: List<String>, searchWord: String) {
+    val word = listOfWords.indexOf(searchWord)
+    println( if (word >= 0) word + 1 else "Not Found")
+}
+
+fun getSearchWord() = readLine()!!
+
+fun getListOfWords() = readLine()!!.split(" ")
